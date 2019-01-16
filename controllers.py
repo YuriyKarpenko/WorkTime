2998a0296d5edc46dbd8de650c61d66aad9fe115
yk_it <molodoy@li.ru>
2019-01-16 17:16:25 +0200

    update options format, models

--- controllers.py
+++ controllers.py
@@ -1,71 +1,100 @@
-from db import Db 
+from db import Db, _Repo
 import json
-from models import Optiopns, Task, TaskItem
+from models import Optiopns, Task, TaskItem, date_fromisoformat
 
 
 class OptionController:
-    def __init__(self):
-        self._filename = 'options.json'
-        self._options: Optiopns = None
+	def __init__(self):
+		self._filename = 'options.json'
+		self._options: Optiopns = None
 
-    def get(self):
-        if not self._options:
-            self._options = self._load()
-        return self._options
+	def get(self):
+		if not self._options:
+			self._options = self._load()
+		return self._options
 
-    def _load(self):
-        data = Db.load_file(self._filename)
-        if data:
-            res = json.loads(data)
-            return Optiopns(res)
-        return Optiopns()
+	def _load(self):
+		data = OptionController.load_file(self._filename)
+		if data:
+			res = json.loads(data)
+			return Optiopns(res)
+		return Optiopns({'db_path': 'db.sqlite3', 'sources': 'EMail\nSkype\nTrello'})
 
-    def save(self) -> bool:
-        if self._options:
-            s = json.dumps(self._options.to_dict())
-            return Db.save_file(s, self._filename)
-        return False
+	def save(self) -> bool:
+		if self._options:
+			s = json.dumps(self._options.to_dict())
+			return OptionController.save_file(s, self._filename)
+		return False
+
+	@staticmethod
+	def load_file(filename: str) -> str or None:
+		if filename:
+			try:
+				with open(filename, 'rt') as f:
+					data = f.read()
+				return data
+			except FileNotFoundError:
+				pass
+		return None
+
+	@staticmethod
+	def save_file(data: str, filename: str) -> bool:
+		if filename and data:
+			try:
+				with open(filename, 'wt') as f:
+					f.write(data)
+				return True
+			except OSError:
+				raise
+		return False
 
 
 optionController = OptionController()
 
 
-class TaskController:
-    # __slots__ = ('current')
-    db: Db
+class _DbDbController:
+	def __init__(self, repo: _Repo): self._repo = repo
 
-    def __init__(self):
-        self.current: Task = None
-        self._filename = optionController.get() and optionController.get().db_path or 'tasks.json'
-        self.db = Db(self._filename)
+	def refresh(self, entity): return self._repo.db.refresh(entity)
 
-    def get(self, Id):
-        if id:
-            self.current = self.db.repoTask.get(Id)
-            return self.current
-        return None
 
-    def get_list(self) -> tuple:
-        res = self.db.repoTask.get_list()
-        print(res)
-        return tuple(res)
+class TaskController(_DbDbController):
+	# __slots__ = ('current')
+	db: Db
 
-    def insert(self, task: Task):
-        self.current = task
-        if task:
-            self.db.repoTask.insert(task)
+	def __init__(self):
+		self.current: Task = None
+		self._filename = optionController.get() and optionController.get().db_path or 'tasks.json'
+		self.db = Db(self._filename)
+		super(TaskController, self).__init__(self.db.repoTask)
 
-    def update(self, task: Task):
-        self.current = task
-        if task:
-            self.db.repoTask.update(task)
+	def get(self, Id):
+		if id:
+			self.current = self._repo.get(Id)
+			return self.current
+		return None
 
-    # items #########################################
+	def get_list(self) -> tuple:
+		res = self._repo.get_list()
+		# print(__class__, 'get_list', res)
+		return tuple(res)
 
-    def get_item(self, Id):
-        if self.current and Id:
-            return next(i for i in self.current.items if i.id == Id)
-        return None
+	def insert(self, task: Task):
+		self.current = task
+		if task:
+			self.db.repoTask.insert(task)
+
+	def update(self, task: Task):
+		self.current = task
+		if task:
+			self.db.repoTask.update(task)
+
+	# items #########################################
+
+	def get_item(self, Id):
+		if self.current and Id:
+			return next(i for i in self.current.items if i.id == Id)
+		return None
 
 
 taskController = TaskController()
--- models.py
+++ models.py
@@ -1,89 +1,90 @@
-import datetime
+from datetime import date, timedelta
 
 
 def date_fromisoformat(ISOstr: str):
-    ss = ISOstr.split('-', 3)
-    return datetime.date(int(ss[0]), int(ss[1]), int(ss[2]))
+	ss = ISOstr.split('-', 3)
+	return date(int(ss[0]), int(ss[1]), int(ss[2]))
 
-
-def to_dict(o):
-    res = {}
-    for k in o:
-        v = o[k]
-        # t = type(v)
-        # if t.
-        res[k] = v
-    return res
+# TODO: надо разобраться с рефлесией
+# def to_dict(o):
+#     res = {}
+#     for k in o:
+#         v = o[k]
+#         # t = type(v)
+#         # if t.
+#         res[k] = v
+#     return res
 
 
 class Optiopns:
-    __slots__ = ('db_path', 'sources')
-    db_path: str
-    sources: list
+	__slots__ = ('db_path', 'sources')
+	db_path: str
+	sources: str
 
-    def __init__(self, json: dict = None):
-        if json:
-            self.db_path = json.get('db_path') or ''
-            self.sources = json.get('sources') or []
-        else:
-            self.db_path = ''
-            self.sources = []
+	def __init__(self, json: dict = None):
+		if json:
+			self.db_path = json.get('db_path', '')
+			self.sources = json.get('sources', '')
+		else:
+			self.db_path = ''
+			self.sources = ''
 
-    def to_dict(self):
-        return {'db_path': self.db_path, "sources": self.sources}
+	def to_dict(self):
+		return {'db_path': self.db_path, "sources": self.sources}
 
 
 class _model(object):
-    id: int = None
+	def __init__(self):
+		self.id: int = None
+		self.date = date.today()
+		self.title = ''
 
 
 class Task(_model):
-    #__slots__ = ('id', 'date', 'title', 'source', 'description', 'items', 'time')
-    #__slots__ = ('date', 'title', 'source', 'description', 'items', 'time')
+	# __slots__ = ('id', 'date', 'title', 'source', 'description', 'items', 'time')
+	# __slots__ = ('date', 'title', 'source', 'description', 'items', 'time')
 
-    def __init__(self):
-        self.date = datetime.date.today()
-        self.title = ''
-        self.source = ''
-        self.description = ''
+	def __init__(self, *args, **kw):
+		super(Task, self).__init__()
+		self.source = ''
+		self.description = ''
 
-        self.items = [TaskItem(self)]
-        self.time = datetime.timedelta(0)
+		self.items = [TaskItem(self)]
+		self.time = timedelta(0)
 
-    def calc_time(self):
-        r: datetime.timedelta = datetime.timedelta(0)
-        # for i in self.items:
-        #     r += i.time
-        # return r
-        return sum((i.time for i in self.items), r)
+	def calc_time(self):
+		r: timedelta = timedelta(0)
+		# for i in self.items:
+		#     r += i.time
+		# return r
+		return sum((i.time for i in self.items), r)
 
-    def __repr__(self):
-        return "<%s(%i, %s, '%s')>" % (self.__class__.__name__, self.id or 0, self.date, self.title)
+	def __repr__(self):
+		return "<%s(%i, %s, '%s', '%s', '%s', %i)>" % (self.__class__.__name__, self.id or 0, self.date, self.title, self.source, self.description, len(self.items))
 
 
 class TaskItem(_model):
-    #__slots__ = ('id', 'parentId', 'date', 'title', 'solution', 'time')
-    _time: datetime.timedelta
+	# __slots__ = ('id', 'parentId', 'date', 'title', 'solution', 'time')
+	time: timedelta = property(lambda self: timedelta(0, self.time_seconds or 0), lambda self, value: setattr(self, 'time_seconds', (value and value.total_seconds() or 0)))
 
-    def __init__(self, parent: Task):
-        self.date = datetime.date.today()
-        self.title = ''
-        self.solution = ''
-        self.time_seconds = 0
-        #self.time = datetime.timedelta(0)
+	def __init__(self, parent: Task):
+		super(TaskItem, self).__init__()
+		self.solution = ''
+		self.time_seconds = 0
+		# self.time = datetime.timedelta(0)
 
-        self.task_id = parent.id
-        self.task = parent
+		self.task_id = parent.id
+		self.task = parent
 
-    def __str__(self):
-        return '{0} {1}'.format(self.solution, self.time)
+	def __str__(self):
+		return '{0} {1}'.format(self.solution, self.time)
 
-    @property
-    def time(self) -> datetime.timedelta:
-        self._time = datetime.timedelta(0, self.time_seconds or 0)
-        return self._time
-    
-    @time.setter
-    def time(self, value: datetime.timedelta):
-        self._time = value
-        self.time_seconds = value and value.total_seconds() or 0
+	# @property
+	# def time(self) -> timedelta:
+	# 	self._time = timedelta(0, self.time_seconds or 0)
+	# 	return self._time
+	#
+	# @time.setter
+	# def time(self, value: timedelta):
+	# 	self._time = value
+	# 	self.time_seconds = value and value.total_seconds() or 0
